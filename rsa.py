aee39786dc30bb1ec31131b587f1199223a41c2e
anhtn21 <anhtn512@gmail.com>
2018-04-06 09:59:12 +0700

    add rsa
    add a file abount rsa

---
+++ rsa.py
@@ -0,0 +1,22 @@
+def fermat_factor(n):
+    assert n % 2 != 0
+    a = gmpy2.isqrt(n)
+    b2 = gmpy2.square(a) - n
+    while not gmpy2.is_square(b2):
+        a += 1
+        b2 = gmpy2.square(a) - n
+    p = a + gmpy2.isqrt(b2)
+    q = a - gmpy2.isqrt(b2)
+    return int(p), int(q)
+	
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    g, y, x = egcd(b%a,a)
+    return (g, x - (b//a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('No modular inverse')
+    return x%m
